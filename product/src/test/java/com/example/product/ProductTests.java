3611b64cb6940068ee852968173337b7ee4d736a
dukim <dukim@bnpinnovation.com>
2017-04-24 17:34:04 +0900

    product test 생성

--- product/src/test/java/com/example/product/ProductTests.java
+++ product/src/test/java/com/example/product/ProductTests.java
@@ -5,12 +5,30 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.fail;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProductApplicationTests {
+public class ProductTests {
 
 	@Test
-	public void contextLoads() {
+	public void 제품_등록() {
+		fail();
+	}
+
+	@Test
+	public void 제품_조회() {
+		fail();
+	}
+
+	@Test
+	public void 제품_삭제() {
+		fail();
+	}
+
+	@Test
+	public void 제품_수정() {
+		fail();
 	}
 
 }

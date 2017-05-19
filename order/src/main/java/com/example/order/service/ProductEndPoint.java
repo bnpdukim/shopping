a76a169a84391648caf6198609c7774c38030ce5
dukim <dukim@bnpinnovation.com>
2017-05-19 11:22:31 +0900

    sso client 적용

--- order/src/main/java/com/example/order/service/ProductEndPoint.java
+++ order/src/main/java/com/example/order/service/ProductEndPoint.java
@@ -1,6 +1,5 @@
 package com.example.order.service;
 
-import com.example.order.config.FeignConfiguration;
 import com.example.product.dto.ProductDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by sajacaros on 2017-04-27.
  */
-@FeignClient(value = "product")
+@FeignClient(value = "product", fallback = ProductFallback.class)
 public interface ProductEndPoint {
     @RequestMapping(method = RequestMethod.GET, value = "/product/api/v1/{productId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     ResponseEntity<ProductDto.Response> product(@PathVariable("productId") Long productId);
